e6a9d2cde90183cc2e925b27f7f29d54de6b32c1
Cameron Presley <cameron@thesoftwarementor.com>
2016-01-04 12:25:33 -0500

    Initial commit

---
+++ CsvParserDriver/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open CsvParserExample
+[<EntryPoint>]
+let main argv = 
+    let transactions = Library.readFile ()
+    let allVisaUsers = transactions |> List.filter(fun x -> x.paymentType = Models.Visa)
+    allVisaUsers |> List.length |> printfn "There are %d Visa card users"
+    0 // return an integer exit code
---
+++ CsvParserExample/Library1.fs
@@ -0,0 +1,49 @@
+﻿namespace CsvParserExample
+
+#if INTERACTIVE
+#r "bin/Debug/FSharp.Data.dll"
+#endif
+module Models =
+    open System
+    
+    type Payment = Visa | Mastercard | Amex | Diners | Unknown
+    type Transaction = {
+        date:DateTime; 
+        product:string; 
+        price:decimal; 
+        paymentType:Payment; 
+        name:string; 
+        city:string; 
+        state:string; 
+        country:string; 
+        accountCreated:DateTime
+    }
+
+module Library =
+    open Models
+    open FSharp.Data
+    open FSharp.Data.CsvExtensions
+    let private convertStringToPayment (input:string) =
+        match input.ToLower() with
+        | "mastercard" -> Mastercard
+        | "visa" -> Visa
+        | "amex" -> Amex
+        | "diners" -> Diners
+        | _ -> Unknown
+
+    let private convertRowToTransaction (row:CsvRow) = {
+        date=row?Transaction_date.AsDateTime();
+        product=row?Product;
+        price=row?Price.AsDecimal();
+        paymentType=row?Payment_Type |> convertStringToPayment;
+        name=row?Name;
+        city=row?City;
+        state=row?State;
+        country=row?Country;
+        accountCreated=row?Account_Created.AsDateTime()
+    }
+
+    let readFile () =
+        let csv = CsvFile.Load("SalesJan2009.csv").Cache()
+        csv.Rows |> Seq.map(fun x -> convertRowToTransaction x) |> Seq.toList
+      
